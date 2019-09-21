8f61f2136d78bd5296787d015e4392689b55debd
Алексей Талаев <alextalaev@outlook.com>
2019-09-22 00:39:09 +0300

    Тестовое приложение отправки почты

--- Tests/MailSenderTestWPF/MainWindow.xaml.cs
+++ Tests/MailSenderTestWPF/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Net;
+using System.Net.Mail;
+using System.Security;
+using System;
 
 namespace MailSenderTestWPF
 {
@@ -23,6 +14,39 @@ namespace MailSenderTestWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var host = "SMTP.Office365.com";
+            var port = 587;
+            var user_name = UserE.Text;
+            var msg = "Привет !" + DateTime.Now;
+            SecureString Password = PassE.SecurePassword; 
+            using (var client = new SmtpClient(host, port))
+            {
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential(user_name, Password);
+                using (var message = new MailMessage())
+                {
+                    message.From = new MailAddress("alextalaev@outlook.com","Алексей");
+                    message.To.Add(new MailAddress("lera95-14@mail.ru", "Алексей"));
+                    message.Subject = "Сообщение с приветствием" +DateTime.Now;
+                    message.Body = msg;
+
+                    try
+                    {
+                        client.Send(message);
+                        MessageBox.Show("Отправлено", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    } catch (Exception error)
+                    {
+                        MessageBox.Show(error.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+
+                }
+
+            }
         }
     }
 }
