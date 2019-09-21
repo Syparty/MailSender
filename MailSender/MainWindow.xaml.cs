eaa15b99802aac127dba8e77cd2cf58a3667ee06
Алексей Талаев <alextalaev@outlook.com>
2019-09-22 01:43:15 +0300

    Структура приложения

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MailSender
         {
             InitializeComponent();
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
