912d8e8a572abb41d16a6832c8e3f76a4c80ecb4
Алексей Талаев <alextalaev@outlook.com>
2019-09-23 02:37:24 +0300

    Домашнее задание, Этап-1

--- Tests/MailSenderTestWPF/MainWindow.xaml.cs
+++ Tests/MailSenderTestWPF/MainWindow.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using System.Windows;
-using System.Net;
-using System.Net.Mail;
-using System.Security;
-using System;
-
 namespace MailSenderTestWPF
 {
     /// <summary>
@@ -18,35 +13,8 @@ namespace MailSenderTestWPF
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var host = "SMTP.Office365.com";
-            var port = 587;
-            var user_name = UserE.Text;
-            var msg = "Привет !" + DateTime.Now;
-            SecureString Password = PassE.SecurePassword; 
-            using (var client = new SmtpClient(host, port))
-            {
-                client.EnableSsl = true;
-                client.Credentials = new NetworkCredential(user_name, Password);
-                using (var message = new MailMessage())
-                {
-                    message.From = new MailAddress("alextalaev@outlook.com","Алексей");
-                    message.To.Add(new MailAddress("lera95-14@mail.ru", "Алексей"));
-                    message.Subject = "Сообщение с приветствием" +DateTime.Now;
-                    message.Body = msg;
-
-                    try
-                    {
-                        client.Send(message);
-                        MessageBox.Show("Отправлено", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
-
-                    } catch (Exception error)
-                    {
-                        MessageBox.Show(error.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                    }
-
-                }
-
-            }
+            EmailSendServiceClass email = new EmailSendServiceClass();
+            email.Send(SMTPE.Text,PortE.Text,UserE.Text,PassE.Password,AddressE.Text,themE.Text,MailE.Text);
         }
     }
 }
