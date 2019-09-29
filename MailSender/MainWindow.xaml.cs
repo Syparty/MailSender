063c209628104e5ea7d2d4d29cd81420f94510ee
Алексей Талаев <alextalaev@outlook.com>
2019-09-30 01:22:29 +0300

    Урок -2 Продолжение

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MailSender.Controls;
 
 namespace MailSender
 {
@@ -20,19 +21,49 @@ namespace MailSender
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() => InitializeComponent();
+
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-
+            Close();
         }
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
+
+        private void UserControl1_LeftButtonClick(object sender, EventArgs e)
+        {
+            if (!(sender is UserControl1 switcher)) return;
+            MaintabControl.SelectedIndex--;
+            if (MaintabControl.SelectedIndex == 0)
+            {
+                switcher.LeftButtonVisible = false;
+            }
+            else if (MaintabControl.SelectedIndex == 1)
+            {
+                switcher.RightButtonVisible = true;
+            }
+
+
+
+        }
+        private void UserControl1_RightButtonClick(object sender, EventArgs e)
+        {
+            if (!(sender is UserControl1 switcher)) return;
+            MaintabControl.SelectedIndex++;
+            if (MaintabControl.SelectedIndex == 2)
+            {
+                switcher.RightButtonVisible = false;
+            }
+            else if (MaintabControl.SelectedIndex == 1)
+            {
+                switcher.LeftButtonVisible = true;
+            }
+
+        }
+
     }
 }
